de6c3784118a7d359add3971000b1513e4c71f17
Theo Oikonomou <theodoros.oikonomou.ltd@gmail.com>
2016-07-10 00:42:54 +0100

    data.router containerization first stage using spotify/kafka files

--- data.router/README.md
+++ data.router/README.md
@@ -1,1 +1,2 @@
-test
+ #start with
+docker run -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=eval(docker-machine ip job) --env ADVERTISED_PORT=9092 wolf_data_router
--- rule.engine/bin/run.sh
+++ rule.engine/bin/run.sh
@@ -3,22 +3,20 @@
 # do I really need autopurge?
 #sudo echo autopurge.purgeInterval=24 >> /etc/zookeeper/conf/zoo.cfg
 #sudo echo autopurge.snapRetainCount=5 >> /etc/zookeeper/conf/zoo.cfg
-sudo ./other/zookeeper-3.4.8/bin/zkServer.sh start
+sudo /usr/share/zookeeper/bin/zkServer.sh start
 
 # check if zookeeper is running
 echo stat | nc localhost 2181
 
 # run nimbus
-# ./apache-storm-0.9.2-incubating/bin/storm nimbus &
-./other/storm/bin/storm nimbus &
-
+./apache-storm-0.9.2-incubating/bin/storm nimbus &
 sleep 10
 # run storm ui
-./other/apache-storm-1.0.1/bin/storm ui &
+./apache-storm-0.9.2-incubating/bin/storm ui &
 sleep 10
 # run storm supervisor
-./other/apache-storm-1.0.1/bin/storm supervisor &
+./apache-storm-0.9.2-incubating/bin/storm supervisor &
 sleep 10
 # now build topology
-./other/apache-storm-1.0.1/bin/storm jar ./wolf/rule.engine/target/rule.engine-0.0.1-SNAPSHOT-jar-with-dependencies.jar rule.engine.RuleEngineTopology RuleEngine
+./apache-storm-0.9.2-incubating/bin storm jar ./wolf/rule.engine/target/rule.engine-0.0.1-SNAPSHOT-jar-with-dependencies.jar rule.engine.RuleEngineTopology RuleEngine
 
