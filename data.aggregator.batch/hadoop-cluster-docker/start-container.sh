7a5417fdb781497cce24d4d0c69db6ff5a8a4e5f
Theo Oikonomou <theodoros.oikonomou.ltd@gmail.com>
2016-08-04 19:10:05 +0100

    starts datanodes correctly (but in hadoop subnet)

--- data.aggregator.batch/hadoop-cluster-docker/start-container.sh
+++ data.aggregator.batch/hadoop-cluster-docker/start-container.sh
@@ -9,21 +9,22 @@ N=${1:-3}
 # be careful with the links (they must refer to existing containers), docker does error
 docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
-docker run -itd -p 50070:50070 -p 8088:8088 --name hadoop-master --hostname hadoop-master --link wolf_cassandra_1:database --link kafka wolf_hadoop &> /dev/null
+docker run -itd -p 50070:50070 -p 8088:8088 --name hadoop-master --net=hadoop  --hostname hadoop-master --link wolf_cassandra_1:database --link kafka wolf_hadoop &> /dev/null
 
 # start hadoop slave container
-# i=1
-# while [ $i -lt $N ]
-# do
-# 	 docker rm -f hadoop-slave$i &> /dev/null
-# 	 echo "start hadoop-slave$i container..."
-# 	 docker run -itd \
-# 	                --net=hadoop \
-# 	                --name hadoop-slave$i \
-# 	                --hostname hadoop-slave$i \
-# 	                kiwenlau/hadoop:1.0 &> /dev/null
-# 	 i=$(( $i + 1 ))
-# done
+i=1
+while [ $i -lt $N ]
+do
+	 docker rm -f hadoop-slave$i &> /dev/null
+	 echo "start hadoop-slave$i container..."
+	 docker run -itd \
+	                --name hadoop-slave$i \
+                        --net=hadoop \
+                        --link wolf_cassandra_1:database --link kafka \
+	                --hostname hadoop-slave$i \
+	                wolf_hadoop &> /dev/null
+	 i=$(( $i + 1 ))
+done
 
 # get into hadoop master container
-# docker exec -it hadoop-master bash
+docker exec -it hadoop-master bash
