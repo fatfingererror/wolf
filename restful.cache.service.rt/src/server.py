972e1b085129dfe1e7cc43d990ba8518f93c14d7
Theo Oikonomou <theodoros.oikonomou.ltd@gmail.com>
2016-09-02 23:42:56 +0100

    data.aggregator.rt working version

--- restful.cache.service.rt/src/server.py
+++ restful.cache.service.rt/src/server.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python
-
+import os
 from flask import Flask, jsonify
-from thread import start_new_thread
+import threading
 import time, datetime, struct
-import cql
-import pytz
 import socket
 from flask import make_response, request, current_app
 from datetime import timedelta
 from functools import update_wrapper
+from cassandra.cluster import Cluster
 
 def crossdomain(origin=None, methods=None, headers=None,
                 max_age=21600, attach_to_all=True,
                 automatic_options=True):
     if methods is not None:
         methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
+    if headers is not None and not isinstance(headers, str):
         headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
+    if not isinstance(origin, str):
         origin = ', '.join(origin)
     if isinstance(max_age, timedelta):
         max_age = max_age.total_seconds()
@@ -52,48 +51,50 @@ def crossdomain(origin=None, methods=None, headers=None,
     return decorator
 
 # connect to database
-con = cql.connect('ec2-54-187-166-118.us-west-2.compute.amazonaws.com',
-                '9160', 'janusz_forex_rt_demo', cql_version='3.1.1')
-cursor = con.cursor()
+db_configs = os.getenv('DATABASE_PORT_9042_TCP').replace('tcp://', '')
+HOST, PORT = db_configs.split(':')
+cluster = Cluster([HOST], port=PORT)
+session = cluster.connect('wolf')
 
 tasks = {
-	'AUDUSD':0,
-	'EURUSD':0,
-	'GBPUSD':0,
-	'NZDUSD':0,
-	'USDCAD':0,
-	'USDCHF':0,
-	'USDJPY':0
-	}
+    'AUDUSD':0,
+    'EURUSD':0,
+    'GBPUSD':0,
+    'NZDUSD':0,
+    'USDCAD':0,
+    'USDCHF':0,
+    'USDJPY':0
+}
 
-pairs       = ['AUDUSD', 'EURUSD', 'GBPUSD', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY']
+pairs = ['AUDUSD', 'EURUSD', 'GBPUSD', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY']
 
-q       = "select issued_at, ask, bid from ticks where pair_day = :key and issued_at > :utc"
+q = "select issued_at, ask, bid from ticks where pair_day = '%s' and issued_at > '%s'"
 
 def foo(pairs):
-	while True:
-		for p in pairs:
-			date = datetime.datetime.utcnow() - datetime.timedelta(minutes=1)
+    while True:
+        for p in pairs:
+            date = datetime.datetime.utcnow() - datetime.timedelta(minutes=1)
 
-			today = str(date.year) + "-" + str(date.month).zfill(2) + "-" + str(date.day).zfill(2)
-			key   = p + ":" + today
-			utc = today + " " + str(date.hour).zfill(2) + ":" + str(date.minute).zfill(2) 
-			utc = utc + ":" + str(date.second).zfill(2) + "+0000"
-	
-			cursor.execute(q, {"key":key, "utc":utc} )
-		
-			#workaround
-			data = cursor.fetchall()
-			print "got " + str(len(data)) + " results from DB for " + p
-			for d in data:
-				d[0] = str(datetime.datetime.fromtimestamp(
-					struct.unpack('!Q', d[0])[0]/1e3 )
-				)
-			tasks[p] = data
+            today = str(date.year) + "-" + str(date.month).zfill(2) + "-" + str(date.day).zfill(2)
+            key   = p + ":" + today
+            utc = today + " " + str(date.hour).zfill(2) + ":" + str(date.minute).zfill(2)
+            utc = utc + ":" + str(date.second).zfill(2) + "+0000"
 
-		time.sleep(2)
+            data = session.execute(q % (key, utc))
+            print(q % (key, utc))
 
-start_new_thread(foo,(pairs,))
+            #workaround
+            # print("got " + str(len(data)) + " results from DB for " + p)
+            tasks[p] = []
+            for d in data:
+                d[0] = str(datetime.datetime.fromtimestamp(
+                    struct.unpack('!Q', d[0])[0]/1e3 )
+                )
+                tasks[p].append(data)
+
+        time.sleep(2)
+
+threading.Thread(target=foo, args=(pairs,)).start()
 
 app = Flask(__name__)
 
@@ -106,7 +107,8 @@ def get_tasks():
     return jsonify( { 'ticks': tasks } )
 
 if __name__ == '__main__':
-	#app.debug = True
-	app.run(host=socket.gethostbyname(socket.gethostname()))
-	#cursor.close()
-	#con.close()
+    app.debug = True
+    host=socket.gethostbyname(socket.gethostname())
+    app.run(host=host)
+    #cursor.close()
+    #con.close()
